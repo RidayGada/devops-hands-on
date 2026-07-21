402c65ca3a8f52862df3ace5f204678dd6ca2cd7
Riday Gada <riday.gada@indiaideas.com>
2026-07-21 13:02:53 +0530

    hello controller added

---
+++ src/main/java/com/example/student_management/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.student_management.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+	@GetMapping
+	public String helloController() {
+		return "Hello from Hello controller.";
+	}
+}
